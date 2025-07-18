06cd5081b155c740275801a497bd18258e33d6eb
fcosilva@hotmail.com <estefani.tipantunia542@gmail.com>
2025-07-18 16:36:49 -0500

    delete desktop folder

--- Desktop/nuevos proyectos/proyecto final/chatbot-backend/src/huggingface/huggingface.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HuggingFaceService } from './huggingface.service';
-
-@Module({
-  providers: [HuggingFaceService],
-  exports: [HuggingFaceService],
-})
-export class HuggingFaceModule {}
