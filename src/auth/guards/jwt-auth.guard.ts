67aa992ba100e8386930048c268fe52a1bbdb083
fcosilva@hotmail.com <estefani.tipantunia542@gmail.com>
2025-07-19 17:45:14 -0500

    arreglo

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,15 @@
-import { Injectable } from '@nestjs/common';
+
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    if (req.method === 'OPTIONS') {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}
